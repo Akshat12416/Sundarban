0a06d8efde08fc1bc3787d80e82837aec2c8020c
Akshat12416 <akshatgour35@gmail.com>
2025-09-06 18:27:42 +0530

    unnecessary files delete that were no longer needed due to shifting of functionalities on another pages

--- contributor-portal/src/App.jsx
+++ contributor-portal/src/App.jsx
@@ -3,8 +3,8 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import NewProject from "./pages/NewProject";
-import UploadAfter from "./pages/UploadAfter";
-import ProjectStatus from "./pages/ProjectStatus";
+
+
 import MyProjects from "./pages/MyProjects";
 
 export default function App() {
@@ -16,8 +16,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects/new" element={<NewProject />} />
         <Route path="/projects" element={<MyProjects />} />
-        <Route path="/project/:id/upload-after" element={<UploadAfter />} />
-        <Route path="/project/:id/status" element={<ProjectStatus />} />
+
       </Routes>
     </Router>
   );
--- contributor-portal/src/pages/UploadAfter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function UploadAfter() {
-  return (
-    <div>UploadAfter</div>
-  )
-}
-
-export default UploadAfter
